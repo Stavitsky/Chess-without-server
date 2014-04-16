a8c5b6cf631a0655ea66c65acb7d7d235af0d226
Stavitsky <Alexandr.Stavitsky@gmail.com>
2014-04-16 23:33:50 +0400

    add coordinates, change dotting

--- public/js/chess.js
+++ public/js/chess.js
@@ -5,10 +5,10 @@ function CreateBoard (boardHeight, boardWidth) {
 	for (var i = 0; i < boardHeight; i++) {
 		for (var j = 0; j < boardWidth; j++) {
 			if ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) {
-				board.append ('<div class="lightCell"> </div>');
+				board.append ($('<div class="lightCell"> </div>').attr("x",i+1).attr("y",j+1));
 			} else {
-				board.append ('<div class="darkCell"> </div>');
-			}
+				board.append ($('<div class="darkCell"> </div>').attr("x",i+1).attr("y",j+1));
+            }
 		}
 	}
 	//board.css("border: solid 1px black;");
@@ -27,39 +27,40 @@ function Dotting () {
 	king - король
 	*/
 
+//    console.log("jquery.type: "+ JQuery.type(null));
+
 
 
     for (var i = 0; i < 71; i++) {
         cell.eq(i).attr('id',i);         //нумерация ячеек id
     }
 
-	cell.slice (8,16).append('<img class="black pawn" src="'+pathToDark+' P.ico">'); //ряд черных пешек
-	cell.slice (48,56).append('<img class="white pawn"  src="'+pathToLight+' P.ico">'); //ряд белых пешек
 
-	for (var i = 0; i < 8; i++) {
-		if (i == 0 || i == 7) {
-			$(cell.slice (0,8)[i]).append('<img class="black rook" src="' + pathToDark + ' R.ico">');
-			$(cell.slice (56,71)[i]).append('<img class="white rook"  src="' + pathToLight + ' R.ico">');
-		}
-		if (i == 1 || i == 6) {
-			$(cell.slice (0,8)[i]).append('<img class="black knight"  src="' + pathToDark + ' N.ico">');
-			$(cell.slice (56,71)[i]).append('<img class="white knight"  src="' + pathToLight + ' N.ico">');
-		}
-		if (i == 2 || i == 5) {
-			$(cell.slice (0,8)[i]).append('<img class="black bitshop" src="' + pathToDark + ' B.ico">');
-			$(cell.slice (56,71)[i]).append('<img class="white bitshop" src="' + pathToLight + ' B.ico">');
-		}
-		if (i == 3) {
-			$(cell.slice (0,8)[i]).append('<img class="black queen" src="' + pathToDark + ' Q.ico">');
-			$(cell.slice (56,71)[i]).append('<img class="white queen" src="' + pathToLight + ' Q.ico">');
-		}
-		if (i == 4) {
-			$(cell.slice (0,8)[i]).append('<img class="black king" src="' + pathToDark + ' K.ico">');
-			$(cell.slice (56,71)[i]).append('<img class="white king" src="' + pathToLight + ' K.ico">');
-		}
+    //пешки
+    $('[x=2]').append('<img class="black pawn" src="'+pathToDark+' P.ico">'); //на второй строке ставим пешки
+    $('[x=7]').append('<img class="white pawn"  src="'+pathToLight+' P.ico">'); //на седьмой
+    //ладьи
+    $('[x=1][y=1]').append('<img class="black rook" src="' + pathToDark + ' R.ico">');
+    $('[x=1][y=8]').append('<img class="black rook" src="' + pathToDark + ' R.ico">');
+    $('[x=8][y=1]').append('<img class="white rook"  src="' + pathToLight + ' R.ico">');
+    $('[x=8][y=8]').append('<img class="white rook"  src="' + pathToLight + ' R.ico">');
+    //кони
+    $('[x=1][y=2]').append('<img class="black knight"  src="' + pathToDark + ' N.ico">');
+    $('[x=1][y=7]').append('<img class="black knight"  src="' + pathToDark + ' N.ico">');
+    $('[x=8][y=2]').append('<img class="white knight"  src="' + pathToLight + ' N.ico">');
+    $('[x=8][y=7]').append('<img class="white knight"  src="' + pathToLight + ' N.ico">');
+    //слон
+    $('[x=1][y=3]').append('<img class="black knight"  src="' + pathToDark + ' N.ico">');
+    $('[x=1][y=6]').append('<img class="black knight"  src="' + pathToDark + ' N.ico">');
+    $('[x=8][y=3]').append('<img class="white knight"  src="' + pathToLight + ' N.ico">');
+    $('[x=8][y=6]').append('<img class="white knight"  src="' + pathToLight + ' N.ico">');
+    //ферзи
+    $('[x=1][y=4]').append('<img class="black queen" src="' + pathToDark + ' Q.ico">');
+    $('[x=8][y=4]').append('<img class="white queen" src="' + pathToLight + ' Q.ico">');
+    //короли
+    $('[x=1][y=5]').append('<img class="black king" src="' + pathToDark + ' K.ico">');
+    $('[x=8][y=5]').append('<img class="white king" src="' + pathToLight + ' K.ico">');
 
-		
-	}
 }
 
 //проверка ячейки на наличие фигурки
